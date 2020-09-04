71b4d5bbd96314a59c342600b96741636ce3ae5c
Jonasbadboys <jonathanjeanniard@sfr.fr>
2020-09-04 13:24:23 +0200

    Test des differents type de variable

---
+++ OCL/JS/WEB/variable.js
@@ -0,0 +1,22 @@
+let mavariable;
+mavariable = "une chaine de caractere";
+mavariable = 5.5 //float
+mavariable = 1 //int
+
+let mavariable_1, mavariable_2;
+mavariable_1 = 1;
+mavariable_2 = mavariable_1;// à ne pas faire
+mavariable_2 = mavariable_1 = 2//mais 2 varaible vos 2
+
+let vaString = "Bonjour";
+let vaInt = 1;
+let vaFloat = 5.3;
+let vaBool = true;
+
+/*
+Test/montre les types des variables avec le typeof
+*/
+console.log(typeof vaString);
+console.log(typeof vaInt);
+console.log(typeof vaFloat);
+console.log(typeof vaBool);
