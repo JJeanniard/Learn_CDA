63e7776e0a6190717e6421611d019d79e17c41d1
Jonasbadboys <jonathanjeanniard@sfr.fr>
2020-09-09 07:58:41 +0200

    fin Tp

--- OCL/JS/NODEJS/app.js
+++ OCL/JS/NODEJS/app.js
@@ -1,3 +1,4 @@
 //const condition = require("./conditions.js");
 //const func = require("./function.js");
-const func = require("./objtlitt.js");
+//const func = require("./objtlitt.js");
+const func = require("./TP/convert_nb_alpha.js");
